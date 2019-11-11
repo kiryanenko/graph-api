1f60a94887d2b8fe4fbe9546a5beaaed1710e9b6
Alexander <kiryanenkoav@mail.ru>
2019-11-11 23:28:41 +0300

    Добавил тесты к добавлению вершины

--- tests/test_spu_ultra_graph.cpp
+++ tests/test_spu_ultra_graph.cpp
@@ -8,38 +8,53 @@
 #define BOOST_TEST_MODULE testSpuUltraGraph
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+#include <boost/graph/graph_concepts.hpp>
+//#include <boost/graph/graph_test.hpp>
 
 
 BOOST_AUTO_TEST_SUITE(testSuiteSpuUltraGraph)
 
 
-using namespace SPU_GRAPH;
+    using namespace SPU_GRAPH;
+    using namespace boost;
 
 
-struct Fixture
-{
-    Fixture() : graph() {}
+    struct Fixture
+    {
+        Fixture() : graph() {}
 
-    ~Fixture() = default;
+        ~Fixture() = default;
 
-    SpuUltraGraph graph;
-};
+        SpuUltraGraph graph;
+    };
 
 
-BOOST_FIXTURE_TEST_CASE(test_add_vertex, Fixture)
-{
-    auto id = graph.add_vertex();
-    BOOST_CHECK(id > 0);
-    BOOST_CHECK_EQUAL(graph.vertices_count(), 1);
+    BOOST_FIXTURE_TEST_CASE(test_check_concept, Fixture)
+    {
+//        BOOST_CONCEPT_ASSERT(( MutableGraphConcept<SpuUltraGraph> ));
+//        graph_test<SpuUltraGraph> graph_tests;
+//        graph_tests.test_add_vertex(graph);
+    }
 
-    auto id2 = graph.add_vertex();
-    BOOST_CHECK(id2 > 0);
-    BOOST_CHECK(id2 != id);
-    BOOST_CHECK_EQUAL(graph.vertices_count(), 2);
 
-    auto id3 = graph.add_vertex(42);
-    BOOST_CHECK_EQUAL(id3, 42);
-}
+    BOOST_FIXTURE_TEST_CASE(test_add_vertex, Fixture)
+    {
+        auto id = graph.add_vertex();
+        BOOST_CHECK(id > 0);
+        BOOST_CHECK_EQUAL(graph.vertices_count(), 1);
+
+        auto id2 = graph.add_vertex();
+        BOOST_CHECK(id2 > 0);
+        BOOST_CHECK(id2 != id);
+        BOOST_CHECK_EQUAL(graph.vertices_count(), 2);
+
+        auto id3 = graph.add_vertex(3);
+        BOOST_CHECK_EQUAL(id3, 3);
+
+        auto id4 = graph.add_vertex();
+        BOOST_CHECK(id4 > 0);
+        BOOST_CHECK_EQUAL(graph.vertices_count(), 4);
+    }
 
 
 BOOST_AUTO_TEST_SUITE_END()
