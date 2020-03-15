58d58f0ee71f0002f89efa26e6476aabe34edaf0
a.kiryanenko <a.kiryanenko@corp.mail.ru>
2020-03-16 01:16:30 +0300

    Фикс команд nsm и prev у симулятора

--- tests/test_spu_api.cpp
+++ tests/test_spu_api.cpp
@@ -12,6 +12,13 @@ BOOST_AUTO_TEST_SUITE(testSpuApi)
     using namespace SPU;
     using namespace boost;
 
+    struct Fixture
+    {
+        Structure<> structure;
+
+        Fixture() : structure() {}
+        ~Fixture() = default;
+    };
 
     BOOST_AUTO_TEST_CASE(test_mask)
     {
@@ -30,6 +37,42 @@ BOOST_AUTO_TEST_SUITE(testSpuApi)
         mask = FieldsLength<int>::mask(64);
         BOOST_CHECK_EQUAL(mask[0], 0xFFFFFFFF);
         BOOST_CHECK_EQUAL(mask[1], 0xFFFFFFFF);
+    }
+
+
+    BOOST_FIXTURE_TEST_CASE(test_nsm_command, Fixture) {
+        auto resp = structure.nsm(123);
+        BOOST_CHECK_EQUAL(resp.status, ERR);
+
+        structure.insert(5, 55);
+        resp = structure.nsm(123);
+        BOOST_CHECK_EQUAL(resp.status, OK);
+        BOOST_CHECK_EQUAL((int) resp.key, 5);
+        BOOST_CHECK_EQUAL((int) resp.value, 55);
+
+        resp = structure.nsm(3);
+        BOOST_CHECK_EQUAL(resp.status, ERR);
+    }
+
+    BOOST_FIXTURE_TEST_CASE(test_prev_command, Fixture) {
+        auto resp = structure.prev(123);
+        BOOST_CHECK_EQUAL(resp.status, ERR);
+
+        structure.insert(10, 10);
+        resp = structure.prev(10);
+        BOOST_CHECK_EQUAL(resp.status, ERR);
+
+        structure.insert(5, 55);
+        resp = structure.prev(10);
+        BOOST_CHECK_EQUAL(resp.status, OK);
+        BOOST_CHECK_EQUAL((int) resp.key, 5);
+        BOOST_CHECK_EQUAL((int) resp.value, 55);
+
+        resp = structure.prev(123);
+        BOOST_CHECK_EQUAL(resp.status, ERR);
+
+        resp = structure.prev(3);
+        BOOST_CHECK_EQUAL(resp.status, ERR);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
