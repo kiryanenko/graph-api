7ca86066861db5b89efd70d27e3a4340e9912c28
a.kiryanenko <a.kiryanenko@corp.mail.ru>
2020-03-29 19:30:04 +0300

    Перенес реалзиацию в заголовочный файл

--- performance_tests/GraphPerformanceTest.h
+++ performance_tests/GraphPerformanceTest.h
@@ -14,7 +14,7 @@ class GraphPerformanceTest {
     typedef typename graph_traits<G>::edge_descriptor edge_t;
 
 public:
-    void (*test_func)() = nullptr;
+    void (*test_func)(G&) = nullptr;
     string results_file = "results.csv";
     size_t avg_iterations_cnt = 10;
 
@@ -23,20 +23,128 @@ public:
     pair<edge_t, bool> (*add_edge_func)(vertex_t, vertex_t, G&) = add_edge;
 
     size_t start_vertices_cnt = 100;
-    size_t inc_vertices_value = 100;
+    size_t inc_vertices_value = 200;
     size_t end_vertices_cnt = 10000;
     size_t edges_per_vertex = 3;
 
     GraphPerformanceTest() = default;
-    GraphPerformanceTest(void (*test_func)(), string results_file) : test_func(test_func), results_file(std::move(results_file)) {}
-    void start();
+    GraphPerformanceTest(void (*test_func)(G&), string results_file) : test_func(test_func), results_file(std::move(results_file)) {}
+
+    void start() {
+        print_start_info();
+
+        auto start_time = clock();
+        if (should_fill) {
+            auto iterations_cnt = size_t(ceil((end_vertices_cnt - start_vertices_cnt + 0.0) / inc_vertices_value));
+            for (size_t i = 0; i < iterations_cnt; ++i) {
+                auto vertices_cnt = min(start_vertices_cnt + inc_vertices_value * i, end_vertices_cnt);
+
+                cout << time_info() << "Performance iteration " << i + 1 << endl;
+                cout << "---------------------------------------------------------" << endl;
+                cout << "Vertices count = " << vertices_cnt << endl;
+                cout << "Edges count = " << vertices_cnt * edges_per_vertex << endl;
+                cout << "---------------------------------------------------------" << endl << endl;
+
+                auto avg_time = run_avg_tests(vertices_cnt);
+            }
+        } else {
+            auto avg_time = run_avg_tests();
+        }
+        auto end_time = clock();
+
+        cout << "=========================================================" << endl;
+        cout << time_info() << "All tests completed in " << double(end_time - start_time) / CLOCKS_PER_SEC << " seconds." << endl;
+    }
 
 private:
-    void print_start_info();
-    void fill(G &graph, size_t vertices_cnt);
-    double run_avg_tests(size_t vertices_cnt = 0);
-    double run_test(G &graph);
-    string time_info();
+    void print_start_info() {
+        cout << time_info() << "Starting performance test." << endl;
+        cout << "---------------------------------------------------------" << endl;
+        cout << "Results file = " << results_file << endl;
+        cout << "Should fill = " << should_fill << endl;
+        cout << "Is mutable graph test = " << is_mutable_test << endl << endl;
+
+        if (should_fill) {
+            cout << "Start vertices count = " << start_vertices_cnt << endl;
+            cout << "Increment vertices value = " << inc_vertices_value << endl;
+            cout << "End vertices count = " << end_vertices_cnt << endl << endl;
+
+            cout << "Edges per vertex = " << edges_per_vertex << endl;
+            cout << "Start edges count = " << start_vertices_cnt * edges_per_vertex << endl;
+            cout << "Increment edges value = " << inc_vertices_value * edges_per_vertex << endl;
+            cout << "End edges count = " << end_vertices_cnt * edges_per_vertex << endl << endl;
+        }
+
+        auto iterations_cnt = should_fill ? size_t(ceil((end_vertices_cnt - start_vertices_cnt + 0.0) / inc_vertices_value)) : 1;
+        cout << "Performance iterations count = " << iterations_cnt << endl;
+        cout << "AVG iterations count = " << avg_iterations_cnt << endl;
+        cout << "Total tests count = " << iterations_cnt * avg_iterations_cnt << endl;
+        cout << "---------------------------------------------------------" << endl << endl << endl;
+    }
+
+    void fill(G &graph, size_t vertices_cnt) {
+        cout << time_info() << "Start filling a graph..." << endl;
+
+        fill_options<G> opts;
+        opts.add_edge_func = add_edge_func;
+
+        auto start_time = clock();
+        fill_graph(graph, vertices_cnt, vertices_cnt * edges_per_vertex, opts);
+        auto end_time = clock();
+
+        cout << time_info() << "Graph filling completed in " << double(end_time - start_time) / CLOCKS_PER_SEC << " seconds." << endl << endl;
+    }
+
+    double run_avg_tests(size_t vertices_cnt = 0) {
+        G *graph;
+        if (!is_mutable_test) {
+            graph = new G;
+            if (should_fill) {
+                fill(*graph, vertices_cnt);
+            }
+        }
+
+        cout << time_info() << "Starting " << avg_iterations_cnt << " AVG iterations..." << endl << endl;
+        double sum_time = 0;
+        auto start_time = clock();
+        for (size_t i = 0; i < avg_iterations_cnt; ++i) {
+            if (is_mutable_test) {
+                graph = new G;
+                if (should_fill) {
+                    fill(*graph, vertices_cnt);
+                }
+            }
+            auto test_time = run_test(*graph);
+            if (is_mutable_test) {
+                delete graph;
+            }
+
+            cout << time_info() << "Test " << i + 1 << " completed in " << test_time << " seconds." << endl << endl;
+            sum_time += test_time;
+        }
+        auto end_time = clock();
+        if (!is_mutable_test) {
+            delete graph;
+        }
+        auto avg_time = sum_time / avg_iterations_cnt;
+
+        cout << time_info() << avg_iterations_cnt << " AVG iterations completed in " << double(end_time - start_time) / CLOCKS_PER_SEC << " seconds." << endl;
+        cout << avg_iterations_cnt << " AVG test time = " << avg_time << " seconds." << endl << endl;
+        return avg_time;
+    }
+
+    double run_test(G &graph) {
+        auto start_time = clock();
+        test_func(graph);
+        auto end_time = clock();
+        return double(end_time - start_time) / CLOCKS_PER_SEC;
+    }
+
+    string time_info() {
+        auto lt = time(nullptr);
+        auto ptr = localtime(&lt);
+        return asctime(ptr);
+    }
 };
 
 
--- performance_tests/dijkstra.cpp
+++ performance_tests/dijkstra.cpp
@@ -4,16 +4,31 @@
 
 #include "../SpuUltraGraphAdapter.h"
 #include "../SpuUltraGraphProperty.h"
-#include "utils.h"
+#include <boost/graph/dijkstra_shortest_paths_no_color_map.hpp>
+#include "GraphPerformanceTest.h"
 
 
 using namespace SPU_GRAPH;
 using namespace boost;
 
 
+void dijkstra_test(SpuUltraGraph &g) {
+    // Создаю свойство предшественник для вершин
+    map<SpuUltraGraph::vertex_descriptor, SpuUltraGraph::vertex_descriptor> vertex_to_predecessor;
+    associative_property_map<map<SpuUltraGraph::vertex_descriptor, SpuUltraGraph::vertex_descriptor>> predecessor_property_map(vertex_to_predecessor);
+    // Создаю свойство расстояние для вершин
+    map<SpuUltraGraph::vertex_descriptor, size_t> vertex_to_distance;
+    associative_property_map<map<SpuUltraGraph::vertex_descriptor, size_t>> distance_property_map(vertex_to_distance);
+
+    // Выполняю алгоритм дейкстра для подсчета расстояний от вершины #1 до остальных
+    dijkstra_shortest_paths_no_color_map(g, 1, predecessor_map(predecessor_property_map).distance_map(distance_property_map));
+}
+
+
 int main()
 {
-    SpuUltraGraph graph;
-    fill_graph(graph, 100, 200);
+    GraphPerformanceTest<SpuUltraGraph> spu_graph_test(dijkstra_test, "dijkstra_test_SpuUltraGraph.csv");
+    spu_graph_test.is_mutable_test = false;
+    spu_graph_test.start();
     return 0;
 }
